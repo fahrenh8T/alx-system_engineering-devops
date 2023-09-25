3c13880d397fc0fb3d71034cc79a858e1f1676f7
fahrenh8T <raymondrayn98@gmail.com>
2023-09-25 04:20:03 +0300

    Client configuration file (w/ Puppet); 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# puppet script to create ssh config file
+# ensure SSH client configuration allows key-based authentication and refuses password authentication
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+# declare identity file
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
