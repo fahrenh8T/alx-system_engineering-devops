46f0d35f448c480ff1d34e1b9a608b5e747b93be
fahrenh8T <raymondrayn98@gmail.com>
2023-09-27 04:03:38 +0300

    install Nginx w/ Puppet; 7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,14 @@
+# puppet manifest to install nginx
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
