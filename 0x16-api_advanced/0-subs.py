ad6656c405e1e7823e0433d452d48ec910a89ed1
fahrenh8T <raymondrayn98@gmail.com>
2023-11-08 21:16:16 +0300

    how many subs; 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" function to query subscribers on a given Reddit subreddit """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ return the total number of subscribers on a given subreddit """
+    link = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "linux:0x16.api.advanced"}
+    response = requests.get(link, headers=headers, allow_redirects=False)
+
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
